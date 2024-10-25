115dd5f66b6554dd0476e38c6fdcfed153d64d78
FlexaGroup <162819959+FlexaGroup@users.noreply.github.com>
2024-10-25 19:07:32 +0100

    contact possible

---
+++ netlify/functions/contactForm.js
@@ -0,0 +1,44 @@
+// contactForm.js
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed',
+        };
+    }
+
+    const { name, email, message, contactPreference } = JSON.parse(event.body);
+    const webhookURL = process.env.DISCORD_WEBHOOK_URL4;
+
+    const embed = {
+        title: "New contact form",
+        fields: [
+            { name: "Name", value: name || "Not provided" },
+            { name: "Email", value: email || "Not provided" },
+            { name: "Message", value: message || "Not provided" },
+            { name: "Preferred Contact Method", value: contactPreference ? contactPreference.join(", ") : "None" }
+        ],
+    };
+
+    const body = JSON.stringify({ embeds: [embed] });
+
+    try {
+        await fetch(webhookURL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body,
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Form submitted successfully!' }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error sending message' }),
+        };
+    }
+};
