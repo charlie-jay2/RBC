8e7095ad514abd0c97c62d4b63fb49bce172943e
FlexaGroup <162819959+FlexaGroup@users.noreply.github.com>
2024-10-28 05:09:55 +0000

    notibar system

---
+++ netlify/functions/updateNotibar.js
@@ -0,0 +1,60 @@
+const { MongoClient } = require('mongodb');
+
+// Replace with your MongoDB connection URI
+const uri = process.env.MONGODB_URI;
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ error: 'Method not allowed' }),
+        };
+    }
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const database = client.db('RBCBlogs');
+        const collection = database.collection('notibar');
+
+        const data = JSON.parse(event.body); // Parse the incoming data
+
+        // Update the Notibar schema with the new data
+        const result = await collection.findOneAndUpdate(
+            {},
+            {
+                $set: {
+                    text: data.text,
+                    color: data.color,
+                    buttonActive: !!data.buttonText,
+                    buttonText: data.buttonText,
+                    buttonColor: data.buttonColor,
+                    buttonURL: data.buttonURL,
+                    active: data.active
+                }
+            },
+            { returnDocument: 'after' } // Return the updated document
+        );
+
+        if (result.value) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ success: true, updatedNotibar: result.value }),
+            };
+        } else {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ error: 'Updated notibar.' }),
+            };
+        }
+    } catch (error) {
+        console.error('Error occurred:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to update Notibar', details: error.message }),
+        };
+    } finally {
+        await client.close();
+    }
+};
